57db5afe1b12233c0088354b441c7b2427509c4d
SPrio <mahantasupriyo@gmail.com>
2021-01-10 21:08:05 +0530

    updated base app.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
 function App() {
   return (
-    <div className="App">
-     
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+        <main>Product list</main>
+        <footer>All right is reserved.</footer>
+      </header>
     </div>
   );
 }
