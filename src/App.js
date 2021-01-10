ff847240758a76aba1d037f3567d87ed30364f32
SPrio <mahantasupriyo@gmail.com>
2021-01-10 21:53:17 +0530

    base formatting added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from "react";
+
 function App() {
   return (
     <div className="grid-container">
       <header>
         <a href="/">React Shopping Cart</a>
-        <main>Product list</main>
-        <footer>All right is reserved.</footer>
       </header>
+      <main>Product list</main>
+      <footer>All right is reserved.</footer>
     </div>
   );
 }
