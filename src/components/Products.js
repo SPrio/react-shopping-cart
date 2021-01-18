098ccf2f862f9890f7382804d0d2ce5cf17476f6
SPrio <mahantasupriyo@gmail.com>
2021-01-18 21:46:45 +0530

    Added fade effect in products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,24 +1,32 @@
 import React, { Component } from "react";
-import formatCurrency from "../util"
+import formatCurrency from "../util";
+import Fade from "react-reveal/Fade";
 
 export default class Products extends Component {
   render() {
     return (
       <div>
-        <ul className="products">
-          {this.props.products.map((product) => (
-            <li key={product._id}>
-              <a href={"#" + product._id}>
-                <img src={product.image} alt={product.title} />
-                <p>{product.title}</p>
-              </a>
-              <div className="product-price">
-                <div>{formatCurrency(product.price)}</div>
-                <button onClick={() => this.props.addToCart(product)} className="button primary">Add To Cart</button>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <Fade bottom cascade>
+          <ul className="products">
+            {this.props.products.map((product) => (
+              <li key={product._id}>
+                <a href={"#" + product._id}>
+                  <img src={product.image} alt={product.title} />
+                  <p>{product.title}</p>
+                </a>
+                <div className="product-price">
+                  <div>{formatCurrency(product.price)}</div>
+                  <button
+                    onClick={() => this.props.addToCart(product)}
+                    className="button primary"
+                  >
+                    Add To Cart
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </Fade>
       </div>
     );
   }
