8edebf2921ba7b8994e20a198dddbbf90893ef6d
SPrio <mahantasupriyo@gmail.com>
2021-01-19 09:25:15 +0530

    Added modal functionality

--- src/components/Products.js
+++ src/components/Products.js
@@ -24,7 +24,10 @@ export default class Products extends Component {
           <ul className="products">
             {this.props.products.map((product) => (
               <li key={product._id}>
-                <a href={"#" + product._id}>
+                <a
+                  href={"#" + product._id}
+                  onClick={() => this.openModal(product)}
+                >
                   <img src={product.image} alt={product.title} />
                   <p>{product.title}</p>
                 </a>
@@ -41,6 +44,39 @@ export default class Products extends Component {
             ))}
           </ul>
         </Fade>
+        {product && (
+          <Modal isOpen={true}>
+            <Zoom>
+              <button className="close-modal" onClick={this.closeModal}>
+                x
+              </button>
+              <div className="product-details">
+                <img src={product.image} alt={product.title}></img>
+                <div className="product-details-description">
+                  <p>
+                    <strong>{product.title}</strong>
+                  </p>
+                  <p>{product.description}</p>
+                  <p>
+                    Available Sizes:{" "}
+                    {product.availableSizes.map((x) => (
+                      <span>
+                        {" "}
+                        <button className="button">{x}</button>
+                      </span>
+                    ))}
+                  </p>
+                  <div className="product-price">
+                    <div>{formatCurrency(product.price)}</div>
+                    <button className="button primary" onClick={() => {this.props.addToCart(product)
+                    this.closeModal()}}> Add To Cart</button>
+                  </div>
+
+                </div>
+              </div>
+            </Zoom>
+          </Modal>
+        )}
       </div>
     );
   }
